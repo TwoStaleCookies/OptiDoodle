ebcedffd37a06788ac945d0bd37c70675dae9d20
DarkUniter <56057934+TwoStaleCookies@users.noreply.github.com>
2024-03-02 15:42:08 -0800

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import tobii_research as tr
 import time
+import main
 
 found_eyetrackers = tr.find_all_eyetrackers()
 my_eyetracker = found_eyetrackers[0]
@@ -10,10 +11,12 @@ print("Serial number: " + my_eyetracker.serial_number)
 
 #track gaze data
 def gaze_data_callback(gaze_data):
-    print("Left eye: ({gaze_le}) \t Right eye: ({gaze_re})".format(
-        gaze_le = gaze_data['left_gaze_point_on_display_area'],
-        gaze_re = gaze_data['right_gaze_point_on_display_area']
-    ))
+    # print("Left eye: ({gaze_le}) \t Right eye: ({gaze_re})".format(
+    #     gaze_le = gaze_data['left_gaze_point_on_display_area'],
+    #     gaze_re = gaze_data['right_gaze_point_on_display_area']
+    # ))
+    main.main(x = gaze_data['left_gaze_point_on_display_area'], y = gaze_data['right_gaze_point_on_display_area'])
+
 
 my_eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, gaze_data_callback, as_dictionary = True) #start tracking
 time.sleep(5)
