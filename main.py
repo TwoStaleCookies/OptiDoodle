1b07848405aec14e1f9eb7eac6b9ce0912892bbd
hanna <63010501+hhnna@users.noreply.github.com>
2024-03-02 13:54:08 -0800

    mainmainmain

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import colorsys
 import random
 import draw
+from tkinter import *
+from tkinter.ttk import *
 
 #VARIABLES
 colors = ['F9C6C9', 'F2C6DE', 'DBCDF0', 'C6DEF1', 'C9E4DE', 'FAEDCB', 'F7D9C4', 'E2CFC4', 'D2D2CF']
@@ -10,6 +12,7 @@ color = colorsys.BLACK
 blink = False
 xcoor = 0
 ycoor = 0
+squareLen = draw.root.winfo_screenwidth()/100
 
 #CLASSES AND FUNCTIONS
 class Grid:
@@ -21,37 +24,42 @@ class Grid:
 
     def drawGrid(self):
         #draw gridlines
+        for k in range(self.height):
+            Canvas.create_line(0, k * squareLen, draw.root.winfo_screenwidth(), k * squareLen)
+        for m in range(self.width):
+            Canvas.create_line(m * squareLen, 0, m & squareLen, draw.root.winfo_screenwidth())
 
 grid = Grid()
 
 class Square:
     def __init__(self, x, y):
         #width and height are in units of pixels
-        self.length = draw.root.winfo_screenwidth()/100
         self.xcoor = x * self.length
         self.ycoor = y * self.length
 
-    def colorSquare(self):
-        #draw square based on 
+    def colorSquare(self, xcoor, ycoor):
+        #draw square if looked at
+        Canvas.create_rectangle(self.xcoor, self.ycoor, self.xcoor + squareLen, self.ycoor + squareLen, fill=color)
 
 for i in range(grid.height):
     for j in range(grid.width):
         squares.append(Square(i, j))
 
-def color(xcoor, ycoor):
-    #use coordinates to color chosen square
-
 #MAIN CODE
 while True:
     #collect coordinates of eye position
     #1 is temporary value
-    xcoor = 1 / squares[0].length
-    ycoor = 1 / squares[0].length
-    
-    #color square based on coordinate
-    color(xcoor, ycoor)
+    xcoor = 1 / squareLen
+    ycoor = 1 / squareLen
 
-    blink = #boolean if blink is registered
-    if (blink)
+    #color square based on coordinate
+    for i in range(grid.height):
+        for j in range(grid.width):
+            sq = squares[i][j]
+            if (xcoor == sq.xcoor and ycoor == sq.ycoor):
+                sq.colorSquare(xcoor, ycoor)
+
+    blink = False #temporary value
+    if (blink):
         color = colors[random.randint(0,len(colors))]
         blink = False
